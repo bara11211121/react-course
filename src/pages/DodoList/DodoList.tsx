2a0d15a2612a8b7e3cf8c9ed4c1047d35d4dfc40
hryjosn <jo74705@gmail.com>
2025-12-24 20:13:30 +0800

    fix: update linter configuration and improve DodoList component structure

--- src/pages/DodoList/DodoList.tsx
+++ src/pages/DodoList/DodoList.tsx
@@ -1,31 +1,31 @@
 import { useState } from "react";
 
-export const DodoList = () =>{
-    const [inputValue, setInputValue] = useState("");  
-    const [todos, setTodos] =useState([]);
-    const addTodo = () =>{
-        setTodos([...todos, inputValue]);
-        setInputValue("");
-    }
-    
-    return(
-      <div>  
-        <div>It's dodo list</div>
-        <input type = "text" value={inputValue} onChange={(e)=>
-            setInputValue(e.target.value)
-        } />
-        <button onClick={addTodo}>新增</button>
-        <div>
-            
-        </div>
-        <div>
-            <h4>check list here:</h4>
-            <ul>
-                {todos.map((todo,index) =>
-                <ul key={index}>{todo}</ul>
-                )}
-            </ul>
-        </div>
+export const DodoList = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = () => {
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <div>It's dodo list</div>
+      <input
+        onChange={(e) => setInputValue(e.target.value)}
+        type="text"
+        value={inputValue}
+      />
+      <button onClick={addTodo}>新增</button>
+      <div></div>
+      <div>
+        <h4>check list here:</h4>
+        <ul>
+          {todos.map((todo, index) => (
+            <ul key={index}>{todo}</ul>
+          ))}
+        </ul>
       </div>
-    );
+    </div>
+  );
 };
